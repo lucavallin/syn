706aab46bbe50f1a84043d2462eee95163893e60
Luca Cavallin <nillavac@gmail.com>
2021-07-12 23:23:43 +0200

    try labeling images

--- functions/ProcessUpload.go
+++ functions/ProcessUpload.go
@@ -2,10 +2,10 @@
 package p
 
 import (
-	//"cloud.google.com/go/firestore"
-	//"cloud.google.com/go/storage"
-	//vision "cloud.google.com/go/vision/apiv1"
+	"cloud.google.com/go/storage"
+	vision "cloud.google.com/go/vision/apiv1"
 	"context"
+	"fmt"
 	"log"
 )
 
@@ -19,57 +19,40 @@ type GCSEvent struct {
 // ProcessUpload prints a message when a file is changed in a Cloud Storage bucket.
 func ProcessUpload(ctx context.Context, e GCSEvent) error {
 	log.Printf("Processing file: %s", e.Name)
+
+	gcs, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer gcs.Close()
+
+	rc, err := gcs.Bucket(e.Bucket).Object(e.Name).NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	// Creates a client.
+	labeler, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer labeler.Close()
+
+	image, err := vision.NewImageFromReader(rc)
+	if err != nil {
+		return err
+	}
+
+	labels, err := labeler.DetectLabels(ctx, image, nil, 10)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Labels:")
+	for _, label := range labels {
+		fmt.Println(label.Description)
+	}
+
 	return nil
-	//// Creates a client.
-	//client, err := vision.NewImageAnnotatorClient(ctx)
-	//if err != nil {
-	//	log.Fatalf("Failed to create client: %v", err)
-	//}
-	//defer client.Close()
-	//
-	//// Sets the name of the image file to annotate.
-	//filename := "../testdata/cat.jpg"
-	//
-	//file, err := os.Open(filename)
-	//if err != nil {
-	//	log.Fatalf("Failed to read file: %v", err)
-	//}
-	//defer file.Close()
-	//image, err := vision.NewImageFromReader(file)
-	//if err != nil {
-	//	log.Fatalf("Failed to create image: %v", err)
-	//}
-	//
-	//labels, err := client.DetectLabels(ctx, image, nil, 10)
-	//if err != nil {
-	//	log.Fatalf("Failed to detect labels: %v", err)
-	//}
-	//
-	//fmt.Println("Labels:")
-	//for _, label := range labels {
-	//	fmt.Println(label.Description)
-	//}
-	//
-	//// storage
-	//client, err := storage.NewClient(ctx)
-	//if err != nil {
-	//	return nil, fmt.Errorf("storage.NewClient: %v", err)
-	//}
-	//defer client.Close()
-	//
-	//ctx, cancel := context.WithTimeout(ctx, time.Second*50)
-	//defer cancel()
-	//
-	//rc, err := client.Bucket(bucket).Object(object).NewReader(ctx)
-	//if err != nil {
-	//	return nil, fmt.Errorf("Object(%q).NewReader: %v", object, err)
-	//}
-	//defer rc.Close()
-	//
-	//data, err := ioutil.ReadAll(rc)
-	//if err != nil {
-	//	return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
-	//}
-	//fmt.Fprintf(w, "Blob %v downloaded.\n", object)
-	//return data, nil
 }
