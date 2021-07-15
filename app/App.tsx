929841d21aa05ead6fa7028c65381297c79bf79f
Luca Cavallin <nillavac@gmail.com>
2021-07-15 22:11:02 +0200

    add expo example

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -15,10 +17,10 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NativeBaseProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+      </NativeBaseProvider>
     );
   }
 }
